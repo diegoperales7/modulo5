cdd87f3a1f673d0e259f9c880a4981ca46c69b7a
Diego Perales <diegoperales878@gmail.com>
2022-10-07 12:30:32 +0200

    Añadiendo froms para visualizar clases desde administrador

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Sender
+from .models import Receiver
+from .models import Address
+from .models import Packet
+from .models import Status
+from .models import Service
 
 # Register your models here.
+
+admin.site.register(Sender)
+admin.site.register(Receiver)
+admin.site.register(Address)
+admin.site.register(Packet)
+admin.site.register(Status)
+admin.site.register(Service)
+
+
